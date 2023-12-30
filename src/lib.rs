6730d8a23e9015f81c68b521ccd614826fc6b89c
edzer.padilla <edzer.padilla@quasset.com>
2023-12-30 21:58:58 +0800

    minesweeper

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,11 @@
 //pub mod reverse;
 //pub mod gigasec;
 //pub mod clock;
-
 //pub mod armstrong;
-
 //pub mod grains;
-
 //pub mod difference_of_squares;
-
 //pub mod anagram;
+//pub mod space_age;
 
-pub mod space_age;
+
+pub mod minesweeper;
---
+++ src/minesweeper.rs
@@ -0,0 +1,165 @@
+
+fn count_mines(minefield: &[&str], i: u32, j: u32) -> char {
+    let i_range = (i.saturating_sub(1)..=i.saturating_add(1)).filter(|&x| (x as usize) < minefield.len());
+    let j_range = (j.saturating_sub(1)..=j.saturating_add(1)).filter(|&x| (x as usize) < minefield[0].len());
+    
+    let mut count = 0;
+
+    for i in i_range {
+        for j in j_range.clone() {
+            if minefield[i as usize].chars().nth(j as usize).unwrap() == '*' {
+                count += 1;
+            }
+        }
+    }
+
+    if count == 0 {
+        return ' ';
+    }
+
+    char::from_digit(count, 10).unwrap_or(' ')
+}
+
+pub fn annotate(minefield: &[&str]) -> Vec<String> {
+    minefield.iter().enumerate().map(|(i, row)| {
+        row.as_bytes().iter().enumerate().map(|(j, ch)| {
+            match ch {
+                b'*' => '*',
+                _ => count_mines(minefield, i as u32, j as u32),
+            }
+        }).collect::<Vec<char>>().iter().collect::<String>()
+    }).collect::<Vec<String>>()
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    fn remove_annotations(board: &[&str]) -> Vec<String> {
+        board.iter().map(|r| remove_annotations_in_row(r)).collect()
+    }
+    fn remove_annotations_in_row(row: &str) -> String {
+        row.chars()
+            .map(|ch| match ch {
+                '*' => '*',
+                _ => ' ',
+            })
+            .collect()
+    }
+    fn run_test(test_case: &[&str]) {
+        let cleaned = remove_annotations(test_case);
+        let cleaned_strs = cleaned.iter().map(|r| &r[..]).collect::<Vec<_>>();
+        let expected = test_case.iter().map(|&r| r.to_string()).collect::<Vec<_>>();
+        assert_eq!(expected, annotate(&cleaned_strs));
+    }
+    #[test]
+    fn no_rows() {
+        #[rustfmt::skip]
+        run_test(&[
+        ]);
+    }
+    #[test]
+    fn no_columns() {
+        #[rustfmt::skip]
+        run_test(&[
+            "",
+        ]);
+    }
+    #[test]
+    fn no_mines() {
+        #[rustfmt::skip]
+        run_test(&[
+            "   ",
+            "   ",
+            "   ",
+        ]);
+    }
+    #[test]
+    fn board_with_only_mines() {
+        #[rustfmt::skip]
+        run_test(&[
+            "***",
+            "***",
+            "***",
+        ]);
+    }
+    #[test]
+    fn mine_surrounded_by_spaces() {
+        #[rustfmt::skip]
+        run_test(&[
+            "111",
+            "1*1",
+            "111",
+        ]);
+    }
+    #[test]
+    fn space_surrounded_by_mines() {
+        #[rustfmt::skip]
+        run_test(&[
+            "***",
+            "*8*",
+            "***",
+        ]);
+    }
+    #[test]
+    fn horizontal_line() {
+        #[rustfmt::skip]
+        run_test(&[
+            "1*2*1",
+        ]);
+    }
+    #[test]
+    fn horizontal_line_mines_at_edges() {
+        #[rustfmt::skip]
+        run_test(&[
+            "*1 1*",
+        ]);
+    }
+    #[test]
+    fn vertical_line() {
+        #[rustfmt::skip]
+        run_test(&[
+            "1",
+            "*",
+            "2",
+            "*",
+            "1",
+        ]);
+    }
+    #[test]
+    fn vertical_line_mines_at_edges() {
+        #[rustfmt::skip]
+        run_test(&[
+            "*",
+            "1",
+            " ",
+            "1",
+            "*",
+        ]);
+    }
+    #[test]
+    fn cross() {
+        #[rustfmt::skip]
+        run_test(&[
+            " 2*2 ",
+            "25*52",
+            "*****",
+            "25*52",
+            " 2*2 ",
+        ]);
+    }
+    #[test]
+    fn large_board() {
+        #[rustfmt::skip]
+        run_test(&[
+            "1*22*1",
+            "12*322",
+            " 123*2",
+            "112*4*",
+            "1*22*2",
+            "111111",
+        ]);
+    }
+
+
+}
